dc92364b2859e7f99db028c4e4d3b71a45516ab1
Amanda Louzada <amandanuneslouzada@gmail.com>
2020-06-14 18:44:37 -0300

    Extract function amountFor

--- __tests__/statment.test.ts
+++ __tests__/statment.test.ts
@@ -1,14 +1,26 @@
-import { amountFor } from "../index"
+import { amountFor, statement } from "../index"
 
 import plays from '../plays';
 import invoices from '../invoices';
 
-
 describe('Statment', () => {
-    it('amount for', async () => {
+    it('amount for', () => {
         const play = { name: 'Hamlet', type: 'tragedy' };
+        const perf = { playID: 'hamlet', audience: 55 };
+        const thisAmount = 65000;
+        expect(amountFor(play, perf)).toBe(thisAmount);
+    })
 
-        expect(amountFor(play)).toBe(6500);
-
+    it('statment', () => {
+        const expectText = 
+        `Statement for BigCo
+        Hamlet: $650.00 (55 seats)
+        As You Like It: $580.00 (35 seats)
+        Othello: $500.00 (40 seats)
+       Amount owed is $1,730.00
+       You earned 47 credits
+       `;
+       const result = statement(invoices[0], plays);
+        expect(result).toBe(result);
     })
 })
--- index.ts
+++ index.ts
@@ -3,34 +3,15 @@
 import plays from './plays';
 import invoices from './invoices';
 
-function statement(invoice: any, plays: any) {
+export const statement = (invoice: any, plays: any) => {
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `Statement for ${invoice.customer}\n`;
     const format = new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format;
     for (let perf of invoice.performances) {
         const play = plays[perf.playID];
-        let thisAmount = 0;
-        console.log(perf);
-        // Calcula o valor para uma apresentação
-        switch (play.type) {
-            case "tragedy":
-                thisAmount = 40000;
-                if (perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30);
-                }
-                break;
+        let thisAmount = amountFor(play, perf);
 
-            case "comedy":
-                thisAmount = 30000;
-                if (perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
-                }
-                thisAmount += 300 * perf.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${play.type}`);
-        }
         // soma créditos por volume
         volumeCredits += Math.max(perf.audience - 30, 0);
 
@@ -48,8 +29,28 @@ function statement(invoice: any, plays: any) {
 }
 
 
-export const amountFor = (play: any, performances: any): string => {
-    return '';
+export const amountFor = (play: any, perf: any): number => {
+    // Calcula o valor para uma apresentação
+    let thisAmount = 0;
+    switch (play.type) {
+        case "tragedy":
+            thisAmount = 40000;
+            if (perf.audience > 30) {
+                thisAmount += 1000 * (perf.audience - 30);
+            }
+            break;
+
+        case "comedy":
+            thisAmount = 30000;
+            if (perf.audience > 20) {
+                thisAmount += 10000 + 500 * (perf.audience - 20);
+            }
+            thisAmount += 300 * perf.audience;
+            break;
+        default:
+            throw new Error(`unknown type: ${play.type}`);
+    }
+    return thisAmount;
 }
 
 
