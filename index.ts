00d41792a08e0336e7fdd2258005896edf436b31
Amanda Louzada <amandanuneslouzada@gmail.com>
2020-06-14 19:22:46 -0300

    internalizar variável

--- index.ts
+++ index.ts
@@ -21,7 +21,6 @@ export const statement = (invoice: any, plays: any) => {
 
     const format = new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format;
     for (let perf of invoice.performances) {
-        let thisAmount = amountFor(perf);
 
         // soma créditos por volume
         volumeCredits += Math.max(perf.audience - 30, 0);
@@ -31,8 +30,8 @@ export const statement = (invoice: any, plays: any) => {
             volumeCredits += Math.floor(perf.audience / 5);
 
         // exibe a linha para esta requisição
-        result += ` ${playFor(perf).name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
-        totalAmount += thisAmount;
+        result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
+        totalAmount += amountFor(perf);
     }
     result += `Amount owed is ${format(totalAmount / 100)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
